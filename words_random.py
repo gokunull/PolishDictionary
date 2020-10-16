21d7d0cdd2d0b533ec7e8cee914cb7a0152148f8
Vladyslav Zhmurko <anon.hkk@gmail.com>
2020-10-16 21:15:09 +0300

    add comparisons words

--- words_random.py
+++ words_random.py
@@ -1,11 +1,11 @@
 import random
 import time
 import codecs
-words = codecs.open("numbers.txt", "r", "utf_8_sig")
+words = codecs.open("comparisons_words.txt", "r", "utf_8_sig")
 unique_words = list(set(words))
 random.shuffle(unique_words)
 random.SystemRandom().shuffle(unique_words)
 for i in unique_words:
     print(i)
-    time.sleep(10)
+    time.sleep(1)
     
