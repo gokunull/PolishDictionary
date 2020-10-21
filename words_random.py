42dfdea5fc2d7e8cd4b36892707cb3f3b688137b
Vladyslav Zhmurko <anon.hkk@gmail.com>
2020-10-21 15:12:11 +0300

    change folder tree + add color words

--- words_random.py
+++ words_random.py
@@ -1,11 +1,11 @@
 import random
 import time
 import codecs
-words = codecs.open("comparisons_words.txt", "r", "utf_8_sig")
+words = codecs.open("words/general_words.txt", "r", "utf_8_sig")
 unique_words = list(set(words))
 random.shuffle(unique_words)
 random.SystemRandom().shuffle(unique_words)
 for i in unique_words:
     print(i)
-    time.sleep(1)
+    time.sleep(20)
     
