6dfb5238b99942a8a8be004587eef9d9e08476e4
Vladyslav Zhmurko <anon.hkk@gmail.com>
2020-10-31 23:42:01 +0200

    added profession and exceptions words

--- words_random.py
+++ words_random.py
@@ -1,11 +1,11 @@
 import random
 import time
 import codecs
-words = codecs.open("words/general_words.txt", "r", "utf_8_sig")
+words = codecs.open("words/color_words.txt", "r", "utf_8_sig")
 unique_words = list(set(words))
 random.shuffle(unique_words)
 random.SystemRandom().shuffle(unique_words)
 for i in unique_words:
     print(i)
-    time.sleep(20)
+    time.sleep(15)
     
