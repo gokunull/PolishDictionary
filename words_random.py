3c0ae1cbf9fd72e2bacc3cf80e9156d5a37989e4
Vladyslav Zhmurko <anon.hkk@gmail.com>
2020-11-08 14:52:37 +0200

    added logfile for check written words

--- words_random.py
+++ words_random.py
@@ -1,16 +1,22 @@
 import random
 import time
 import codecs
-# list = color_words.txt, comparisons_words.txt,
-# exceptions_words.txt, family_words.txt,
-# general_words.txt, mood_words.txt,
-# numbers.txt, numbers_x10.txt,
-# profession_words.txt, 
-words = codecs.open("words/x10_numbers.txt", "r", "utf_8_sig")
+import datetime
+filename = "general_words"
+logname = datetime.datetime.now().strftime("%Y%m%d-%H.%M.%S")
+logfile = codecs.open(f"C:/Other/PythonProject/dictionary/log/{logname}.txt", "w", "utf_8_sig")
+words = codecs.open(f"C:/Other/PythonProject/dictionary/words/{filename}.txt", "r", "utf_8_sig")
 unique_words = list(set(words))
 random.shuffle(unique_words)
 random.SystemRandom().shuffle(unique_words)
 for i in unique_words:
     print(i)
-    time.sleep(15)
-    
+    time.sleep(0.1)
+logfile.writelines("%s\n" % i for i in unique_words)
+logfile.close()
+
+# list = color_words.txt, comparisons_words.txt,
+# exceptions_words.txt, family_words.txt,
+# general_words.txt, mood_words.txt,
+# numbers.txt, numbers_x10.txt,
+# profession_words.txt, ...
