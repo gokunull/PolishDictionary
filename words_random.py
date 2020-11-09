c43e199fd9524e46df61e98a373c5d0f4ffeeeca
Vladyslav Zhmurko <anon.hkk@gmail.com>
2020-11-09 13:52:56 +0200

    change words list

--- words_random.py
+++ words_random.py
@@ -2,9 +2,11 @@ import random
 import time
 import codecs
 import datetime
-filename = "general_words"
-logname = datetime.datetime.now().strftime("%Y%m%d-%H.%M.%S")
-logfile = codecs.open(f"C:/Other/PythonProject/dictionary/log/{logname}.txt", "w", "utf_8_sig")
+wordslist = ['color_words','comparisons_words','exceptions_words','family_words','general_words',
+            'mood_words','numbers','numbers_x10','profession_words']
+filename = wordslist[0]
+logname = datetime.datetime.now().strftime("%m.%d-%H;%M;%S")
+logfile = codecs.open(f"C:/Other/PythonProject/dictionary/log/{logname}_{filename}.txt", "w", "utf_8_sig")
 words = codecs.open(f"C:/Other/PythonProject/dictionary/words/{filename}.txt", "r", "utf_8_sig")
 unique_words = list(set(words))
 random.shuffle(unique_words)
