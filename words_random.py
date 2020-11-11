63c4876fab220071cb76671f62c22e7df9e6b8e4
Vladyslav Zhmurko <anon.hkk@gmail.com>
2020-11-11 21:39:49 +0200

    edit family words

--- words_random.py
+++ words_random.py
@@ -4,7 +4,7 @@ import codecs
 import datetime
 wordslist = ['color_words','comparisons_words','exceptions_words','family_words','general_words',
             'mood_words','numbers','numbers_x10','profession_words']
-filename = wordslist[0]
+filename = wordslist[3]
 logname = datetime.datetime.now().strftime("%m.%d-%H;%M;%S")
 logfile = codecs.open(f"C:/Other/PythonProject/dictionary/log/{logname}_{filename}.txt", "w", "utf_8_sig")
 words = codecs.open(f"C:/Other/PythonProject/dictionary/words/{filename}.txt", "r", "utf_8_sig")
@@ -13,7 +13,7 @@ random.shuffle(unique_words)
 random.SystemRandom().shuffle(unique_words)
 for i in unique_words:
     print(i)
-    time.sleep(0.1)
+    time.sleep(25)
 logfile.writelines("%s\n" % i for i in unique_words)
 logfile.close()
 
