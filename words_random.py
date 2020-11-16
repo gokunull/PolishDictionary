b2175c201d29d7c4059fc8f76e7b6ccbc69d334d
Vladyslav Zhmurko <anon.hkk@gmail.com>
2020-11-16 10:16:33 +0200

    added hobby words

--- words_random.py
+++ words_random.py
@@ -3,7 +3,7 @@ import time
 import codecs
 import datetime
 wordslist = ['color_words','comparisons_words','exceptions_words','family_words','general_words',
-            'mood_words','numbers','numbers_x10','profession_words']
+            'mood_words','numbers','numbers_x10','profession_words', 'hobby_words']
 filename = wordslist[3]
 logname = datetime.datetime.now().strftime("%m.%d-%H;%M;%S")
 logfile = codecs.open(f"C:/Other/PythonProject/dictionary/log/{logname}_{filename}.txt", "w", "utf_8_sig")
@@ -13,7 +13,7 @@ random.shuffle(unique_words)
 random.SystemRandom().shuffle(unique_words)
 for i in unique_words:
     print(i)
-    time.sleep(25)
+    time.sleep(17)
 logfile.writelines("%s\n" % i for i in unique_words)
 logfile.close()
 
@@ -21,4 +21,4 @@ logfile.close()
 # exceptions_words.txt, family_words.txt,
 # general_words.txt, mood_words.txt,
 # numbers.txt, numbers_x10.txt,
-# profession_words.txt, ...
+# profession_words.txt, hobby_words.txt, ...
