daf48ea91421fcd052f95d7070fffba61c3f39ba
Vladyslav Zhmurko <anon.hkk@gmail.com>
2020-11-18 17:27:23 +0200

    added days + shops words

--- words_random.py
+++ words_random.py
@@ -3,7 +3,8 @@ import time
 import codecs
 import datetime
 wordslist = ['color_words','comparisons_words','exceptions_words','family_words','general_words',
-            'mood_words','numbers','numbers_x10','profession_words', 'hobby_words']
+            'mood_words','numbers','numbers_x10','profession_words', 'hobby_words', 'shops_words',
+            'days_words', 'time_of_day_words']
 filename = wordslist[3]
 logname = datetime.datetime.now().strftime("%m.%d-%H;%M;%S")
 logfile = codecs.open(f"C:/Other/PythonProject/dictionary/log/{logname}_{filename}.txt", "w", "utf_8_sig")
@@ -21,4 +22,5 @@ logfile.close()
 # exceptions_words.txt, family_words.txt,
 # general_words.txt, mood_words.txt,
 # numbers.txt, numbers_x10.txt,
-# profession_words.txt, hobby_words.txt, ...
+# profession_words.txt, hobby_words.txt, shops_words.txt,
+# days_words.txt, time_of_day_words.txt, ...
