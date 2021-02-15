97d716a3024f97d591a027d84d72dd9f0b37f5e6
Vladyslav Zhmurko <anon.hkk@gmail.com>
2021-02-15 17:07:16 +0200

    added human words

--- words_random.py
+++ words_random.py
@@ -5,8 +5,8 @@ import datetime
 wordslist = ['color_words','comparisons_words','exceptions_words','family_words','general_words',
             'mood_words','numbers','numbers_x10','profession_words', 'hobby_words', 'shops_words',
             'days_words', 'time_of_day_words', 'food_words', 'city_words', 'route_words', 'weekend_words',
-            'house_words', 'furniture_words', 'kitchen_words']
-filename = wordslist[16]
+            'house_words', 'furniture_words', 'kitchen_words', 'bathroom_words', 'human_words']
+filename = wordslist[20]
 logname = datetime.datetime.now().strftime("%m.%d-%H;%M;%S")
 logfile = codecs.open(f"C:/Other/PythonProject/dictionary/log/{logname}_{filename}.txt", "w", "utf_8_sig")
 words = codecs.open(f"C:/Other/PythonProject/dictionary/words/{filename}.txt", "r", "utf_8_sig")
@@ -26,4 +26,5 @@ logfile.close()
 # profession_words.txt, hobby_words.txt, shops_words.txt,
 # days_words.txt, time_of_day_words.txt, food_words.txt,
 # city_words.txt, route_words.txt, weekend_words.txt,
-# house_words.txt, furniture_words.txt, kitchen_words.txt...
+# house_words.txt, furniture_words.txt, kitchen_words.txt,
+# bathroom_words.txt, human_words.txt...
